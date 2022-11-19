04a27ff46ac76ce3e627f833cbe4c41e2bcd3632
Lucas Couto <loc.unb@gmail.com>
2022-11-19 19:59:00 -0300

    Adds state to close and open modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
+
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHander = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHander} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
